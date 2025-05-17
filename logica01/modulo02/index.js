73b327e3e3730bd940502bbe2c3c79d1fbba24b1
jvlimadev <jvlima_dev1999@outlook.com>
2025-05-17 18:47:26 -0300

    Mudando a extensão dos arquivos para JS

---
+++ logica01/modulo02/index.js
@@ -0,0 +1,5 @@
+<meta charset="UTF-8" />;
+
+document.write("Beatriz nasceu em " + (2025 - 23) + "<br>");
+document.write("João nasceu em " + (2025 - 26) + "<br>");
+document.write("Gustavo nasceu em " + (2025 - 17) + "<br>");
