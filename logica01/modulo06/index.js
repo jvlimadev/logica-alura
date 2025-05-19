c051a940ecb2570abb4d84fe3ca7176aea43da0a
jvlimadev <jvlima_dev1999@outlook.com>
2025-05-19 20:25:11 -0300

    Atualizando o tipo de dados do prompt para ParseInt, corrigindo a soma total dos pontos

--- logica01/modulo06/index.js
+++ logica01/modulo06/index.js
@@ -7,8 +7,8 @@ function mostrar(frase) {
     pularLinha();
 }
 
-var vitorias = prompt('Informe o número de vitórias: ');
-var empates = prompt('Informe o número de empates: ');
+var vitorias = parseInt(prompt('Informe o número de vitórias: '));
+var empates = parseInt(prompt('Informe o número de empates: '));
 var pontos = vitorias * 3 + empates;
 
 mostrar(pontos);
