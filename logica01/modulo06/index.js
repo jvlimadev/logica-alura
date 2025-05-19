594a8a603b80d81713901f29eaa102762cd6cafc
jvlimadev <jvlima_dev1999@outlook.com>
2025-05-19 20:23:19 -0300

    Programa do cálculo de pontos do time

--- logica01/modulo06/index.js
+++ logica01/modulo06/index.js
@@ -7,3 +7,6 @@ function mostrar(frase) {
     pularLinha();
 }
 
+var vitorias = prompt('Informe o número de vitórias: ');
+var empates = prompt('Informe o número de empates: ');
+var pontos = vitorias * 3 + empates;
