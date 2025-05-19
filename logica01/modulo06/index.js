fbee2e08c97745916a680483f2b1f933dc1e5a74
jvlimadev <jvlima_dev1999@outlook.com>
2025-05-19 20:28:35 -0300

    Aplicando condicionais

--- logica01/modulo06/index.js
+++ logica01/modulo06/index.js
@@ -1,14 +1,22 @@
 function pularLinha() {
-    document.write("<br>");
+  document.write("<br>");
 }
 
 function mostrar(frase) {
-    document.write(frase);
-    pularLinha();
+  document.write(frase);
+  pularLinha();
 }
 
-var vitorias = parseInt(prompt('Informe o número de vitórias: '));
-var empates = parseInt(prompt('Informe o número de empates: '));
+var vitorias = parseInt(prompt("Informe o número de vitórias: "));
+var empates = parseInt(prompt("Informe o número de empates: "));
 var pontos = vitorias * 3 + empates;
 
-mostrar(pontos);
+if (pontos > 28) {
+  mostrar("Seu time está melhor do que o ano passado.");
+}
+if (pontos < 28) {
+  mostrar("Seu time está pior do que o ano passado.");
+}
+if (pontos == 28) {
+  mostrar("Seu time está igual ao ano passado.");
+}
