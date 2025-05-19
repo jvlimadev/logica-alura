c54f4efdae25463409136d42ced9022b89af36d0
jvlimadev <jvlima_dev1999@outlook.com>
2025-05-19 20:24:00 -0300

    Mostrando o valor total

--- logica01/modulo06/index.js
+++ logica01/modulo06/index.js
@@ -10,3 +10,5 @@ function mostrar(frase) {
 var vitorias = prompt('Informe o número de vitórias: ');
 var empates = prompt('Informe o número de empates: ');
 var pontos = vitorias * 3 + empates;
+
+mostrar(pontos);
