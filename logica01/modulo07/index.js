ba419db91b3a4d52cd9d9104a44b2d28350fe097
jvlimadev <jvlima_dev1999@outlook.com>
2025-05-20 22:21:43 -0300

    Aplicando as condicionais ao projeto

--- logica01/modulo07/index.js
+++ logica01/modulo07/index.js
@@ -1,2 +1,10 @@
 var numeroPensado = 5;
 var chute = parseInt(prompt('Digite seu chute: '));
+
+if(chute == numeroPensado) {
+    console.log('Você Acertou!');
+}
+
+if(chute !== numeroPensado) {
+    console.log('Você Errou! o número pensado foi ' + numeroPensado);
+}
