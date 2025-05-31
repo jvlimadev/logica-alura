5bd7060fe93150ad3a1754adcd627073133b2f5f
jvlimadev <jvlima_dev1999@outlook.com>
2025-05-31 18:42:46 -0300

    Lista de números acrescentado ao jogo

--- logica01/modulo09/index.js
+++ logica01/modulo09/index.js
@@ -1,18 +1,24 @@
-let segredos = [1,2,3,5];
+let segredos = [1, 2, 3, 5];
 let input = document.querySelector("input");
 input.focus();
 
 function verificar() {
-    
-  for(let i = 0; i < segredos.length; i++) {
+  let achou = false;
+
+  for (let i = 0; i < segredos.length; i++) {
     if (input.value == segredos[i]) {
       alert("Você Acertou!");
+      achou = true;
       break;
     }
   }
-  input.value = '';
+
+  if (achou == false) {
+    alert("Você Errou!");
+  }
+  input.value = "";
   input.focus();
 }
 
-var botao = document.querySelector('button');
+var botao = document.querySelector("button");
 botao.onclick = verificar;
